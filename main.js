52761d4100b8fab31a41c3c0953e72c0f791ce02
cook-brook <72130874+cook-brook@users.noreply.github.com>
2021-07-17 17:01:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 function setup(){
-    canvas=createcanvas(300,300);
+    video=createCapture(VIDEO);
+    video.size(550,650);
+
+    canvas=createCanvas(450,450);
+    canvas.position(560,170);
+
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function draw(){
+    background('#808080');
+}
+function modelLoaded(){
+    console.log('pposenet inishelized')
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
 }
